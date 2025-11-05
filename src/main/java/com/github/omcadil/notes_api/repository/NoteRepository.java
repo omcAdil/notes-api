2b5971ed138bdab0cb0f94ebe2bd8515f5bded2b
Adil Zhumabekov <adil.zhumabekov1@gmail.com>
2025-11-05 10:14:14 +0500

    Рефакторинг: добавлена аннотация репозитиория

--- src/main/java/com/github/omcadil/notes_api/repository/NoteRepository.java
+++ src/main/java/com/github/omcadil/notes_api/repository/NoteRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
     @Query("select n from Note n " +
             "where (:q is null or lower(n.title) like lower(concat('%', :q, '%')) " +
